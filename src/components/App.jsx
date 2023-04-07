6b84de0dc0bd2e5095d5b209d0ed71eff68bd34f
n-ruzhik <anastasiyaruzhetskaya@gmail.com>
2023-04-07 22:07:32 +0300

    task 2 done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,22 @@
 import Profile from 'components/Profile';
+import Statistics from './Statistics/Statistics';
+
 import userData from 'data/user.json';
+import dataData from 'data/data.json';
+
 
 export default function App() {
   return (
+    <>
     <Profile
-        name={userData.username}
-        tag={userData.tag}
-        location={userData.location}
-        avatar={userData.avatar}
-        stats={userData.stats}
+      name={userData.username}
+      tag={userData.tag}
+      location={userData.location}
+      avatar={userData.avatar}
+      stats={userData.stats}
     />
+      <Statistics stats={dataData} />
+      </>
   );
 };
 
